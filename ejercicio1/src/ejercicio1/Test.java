2125be61a6f8cd5f223804c0ef4815e1c11cbd07
Jose Hernandez <jose.hernandez@unosquare.com>
2019-09-26 17:43:30 -0500

    Primer commit

---
+++ ejercicio1/src/ejercicio1/Test.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ejercicio 1 Luis");
+
+	}
+	
+
+}
